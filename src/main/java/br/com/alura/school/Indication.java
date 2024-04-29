1d8c048f266ddd866dbf6e0c77943ab8a98f21c3
Luiz Filipe <luizfilipemr17@gmail.com>
2024-04-29 18:42:19 -0300

    Adding Indication entity

---
+++ src/main/java/br/com/alura/school/Indication.java
@@ -0,0 +1,13 @@
+package br.com.alura.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class Indication {
+
+    private Student indicator;
+    private Student indicated;
+
+}
